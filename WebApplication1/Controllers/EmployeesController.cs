affe01897d8dc4582ebba22be126a7199e3e41e5
morheb bek <mbek@netways.com>
2020-11-11 13:01:16 +0200

    post ad get

---
+++ WebApplication1/Controllers/EmployeesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+	public class EmployeesController : Controller
+				
+	{
+		[Route("api/[controller]")]
+		[HttpPost]
+		
+		public ActionResult<SubmissionResponse> SaveEmployee ([ FromBody]Employees employees)
+		{
+			if (employees == null || string.IsNullOrWhiteSpace(employees.Name)) {
+
+				return BadRequest(new SubmissionResponse
+				{
+					Success = false,
+					ErrorCode = "invalid"
+				});
+
+
+				
+
+			}
+			return Ok(new SubmissionResponse
+			{
+				Success = true
+			});
+		}
+		[HttpGet()]
+		[Route("api/[controller]/{name}")]
+
+		
+		public ActionResult<string> Get()
+		{
+			return ("employees get");
+		}
+	}
+}
